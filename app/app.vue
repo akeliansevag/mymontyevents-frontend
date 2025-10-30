721b6f1f08c0b0dbde9b407e5c4c96e45b65d3f7
akeliansevag <akelian.sevag@gmail.com>
2025-10-30 15:36:23 +0200

    .

--- app/app.vue
+++ app/app.vue
@@ -6,3 +6,14 @@
   </div>
   
 </template>
+
+<script setup>
+  useSeoMeta({
+    title: 'MyMonty Events',
+    ogTitle: 'MyMonty Events',
+    description: 'This is my amazing site, let me tell you all about it.',
+    ogDescription: 'This is my amazing site, let me tell you all about it.',
+    ogImage: 'https://example.com/image.png',
+    twitterCard: 'summary_large_image',
+  })
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      title: 'MyMonty Events', // default fallback title
+      htmlAttrs: {
+        lang: 'en',
+      },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
 })
